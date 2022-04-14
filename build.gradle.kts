e48be078d0be69600f3885728f0955e3998200cd
Luismi Barcos <luismiguel.barcos@liferay.com>
2022-04-14 17:18:02 +0200

    Add spring-hateoas dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ repositories {
 }
 
 dependencies {
+    implementation ("org.springframework.hateoas:spring-hateoas")
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
