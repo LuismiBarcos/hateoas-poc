ea077f6dd225f5539b598edf3da4e8b8a584f127
Luismi Barcos <luismiguel.barcos@liferay.com>
2022-04-14 13:21:39 +0200

    First commit

---
+++ src/test/kotlin/com/poc/hateoas/HateoasApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.poc.hateoas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HateoasApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
